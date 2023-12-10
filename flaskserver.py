f72d79a06b777a8743a34b7baee2c84eb80ce917
Humza <139543666+humzak711@users.noreply.github.com>
2023-12-10 13:54:09 +0000

    Update flaskserver.py
    improving my HTML and CSS

--- flaskserver.py
+++ flaskserver.py
@@ -9,31 +9,35 @@ def frontpage():
     # render HTML on the page
     return '''    
 <html>
-<div style="
-    text-align: center;
-    background-color: blue;
-    color: white;
-    width: 100%;
-    height: 100%;
-    ">
+<style> 
+    html {
+    background-color: blue; width: 100%; height: 100%;
+    text-align: center; color: white; font-family: sans-serif; font-size: 20px; 
+    }
+</style>
     
+    <!DOCTYPE html>
+<html>
     <head>
         <title> First web server </title>
-        <h1> <br> <u> First web server </u> </h1>
-        <p1> Hey! <br> This is my first web server </p1>
+        <h1> <br> <u> This is my first web server </u> </h1>
     </head>
 
     <body>
         <header>
-            <h1> <b> Come back later! </b> </h1>
+            <p1> <b> Come back later! </b> </p1>
         </header>
 
         <main>
-            <img src="https://th.bing.com/th/id/OIP.vyeL1KIJpbGdWaeHh5HfDAHaEq?w=270&h=180&c=7&r=0&o=5&dpr=2&pid=1.7" 
-                 width="400" height="300">
+            <img src="https://th.bing.com/th/id/OIP.vyeL1KIJpbGdWaeHh5HfDAHaEq?w=288&h=181&c=7&r=0&o=5&dpr=2&pid=1.7" 
+            width:"300" height:"350">
         </main>
+
+        <footer>
+            <script src="https://tryhackme.com/badge/2315259"> </script>
+        </footer>
     </body>
-</div>
+</html>
 '''
 
 @app.route('/home') # /home redirects to front page
