1e7e871a02c0ae74552719dfd184dfeb4f68d2aa
Humza <139543666+humzak711@users.noreply.github.com>
2024-01-09 20:48:26 +0000

    Update and rename FlaskWebApp.py to config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+db_hostname = input("input database host: ")
+db_username = input("input database username: ")
+db_password = input("input database password: ")
+db_database = input("input database name: ")
