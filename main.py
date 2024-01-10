9fd4a95df9f097a8acf4fe26bab0393bc7480da1
Humza <139543666+humzak711@users.noreply.github.com>
2024-01-10 00:03:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
-from blueprints.Dashboard import Dashboard_blueprint
-from blueprints.AccountCreation import AccountCreation_blueprint
+from flask import Flask
 from blueprints.Home import Home_blueprint
+from blueprints.AccountCreation import AccountCreation_blueprint
+from blueprints.AccountRecovery import AccountRecovery_blueprint
+from blueprints.Dashboard import Dashboard_blueprint
 
 # Create the Flask application
 app = Flask(__name__)  
-app.secret_key = 'wo2aa38euhwugegwfr235ef36ewg827mawsvwgey3524eudnskjww'
+app.secret_key = input("input apps secret key: ")
 
 # Register blueprints
 app.register_blueprint(Home_blueprint) # Front page
-app.register_blueprint(AccountCreation_blueprint) # Signup, Login, Account recovery
+app.register_blueprint(AccountCreation_blueprint) # Signup/Login
+app.register_blueprint(AccountRecovery_blueprint) # Password reset
 app.register_blueprint(Dashboard_blueprint) # User dashboard
 
 # Run the web application
